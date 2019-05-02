3416c4a4204792d63ff3afcf052451f48767cc3e
OxfordUniversityItalianSociety <italy.society@studentclubs.ox.ac.uk>
2019-05-02 15:22:58 -0230

    Switched to formspree which offers recaptcha against bots. Hope this'll help

--- themes/meghna/assets/js/script.js
+++ themes/meghna/assets/js/script.js
@@ -74,126 +74,126 @@ jQuery(function ($) {
 	/* ========================================================================= */
 
 
-	$('#contact-submit').click(function (e) {
+	// $('#contact-submit').click(function (e) {
 
-		//stop the form from being submitted
-		e.preventDefault();
+	// 	//stop the form from being submitted
+	// 	e.preventDefault();
 
-		/* declare the variables, var error is the variable that we use on the end
-		to determine if there was an error or not */
-		var error = false;
-		var name = $('#name').val();
-		var email = $('#email').val();
-		var subject = $('#subject').val();
-		var message = $('#message').val();
-		var e_mail = $('#e_mail').val();
+	// 	/* declare the variables, var error is the variable that we use on the end
+	// 	to determine if there was an error or not */
+	// 	var error = false;
+	// 	var name = $('#name').val();
+	// 	var email = $('#email').val();
+	// 	var subject = $('#subject').val();
+	// 	var message = $('#message').val();
+	// 	var e_mail = $('#e_mail').val();
 
-		/* in the next section we do the checking by using VARIABLE.length
-		where VARIABLE is the variable we are checking (like name, email),
-		length is a JavaScript function to get the number of characters.
-		And as you can see if the num of characters is 0 we set the error
-		variable to true and show the name_error div with the fadeIn effect.
-		if it's not 0 then we fadeOut the div( that's if the div is shown and
-		the error is fixed it fadesOut.
+	// 	/* in the next section we do the checking by using VARIABLE.length
+	// 	where VARIABLE is the variable we are checking (like name, email),
+	// 	length is a JavaScript function to get the number of characters.
+	// 	And as you can see if the num of characters is 0 we set the error
+	// 	variable to true and show the name_error div with the fadeIn effect.
+	// 	if it's not 0 then we fadeOut the div( that's if the div is shown and
+	// 	the error is fixed it fadesOut.
 
-		The only difference from these checks is the email checking, we have
-		email.indexOf('@') which checks if there is @ in the email input field.
-		This JavaScript function will return -1 if no occurrence have been found.*/
-		if (name.length == 0) {
-			var error = true;
-			$('#name').css("border-color", "#D8000C");
-		} else {
-			$('#name').css("border-color", "#666");
-		}
-		if (email.length == 0 || email.indexOf('@') == '-1') {
-			var error = true;
-			$('#email').css("border-color", "#D8000C");
-		} else {
-			$('#email').css("border-color", "#666");
-		}
-		if (subject.length == 0) {
-			var error = true;
-			$('#subject').css("border-color", "#D8000C");
-		} else {
-			$('#subject').css("border-color", "#666");
-		}
-		if (message.length == 0) {
-			var error = true;
-			$('#message').css("border-color", "#D8000C");
-		} else {
-			$('#message').css("border-color", "#666");
-		}
-		if (e_mail.length != 0) {
-			// bot found!
-			var error = true;
-			// but pretend everything went smoothly
-			$('#contact-submit').attr({
-				'disabled': 'false',
-				'value': 'Sending Mail...'
-			});
-			$('#cf-submit').remove();
-			$('#mail-success').fadeIn(500);
-		}
+	// 	The only difference from these checks is the email checking, we have
+	// 	email.indexOf('@') which checks if there is @ in the email input field.
+	// 	This JavaScript function will return -1 if no occurrence have been found.*/
+	// 	if (name.length == 0) {
+	// 		var error = true;
+	// 		$('#name').css("border-color", "#D8000C");
+	// 	} else {
+	// 		$('#name').css("border-color", "#666");
+	// 	}
+	// 	if (email.length == 0 || email.indexOf('@') == '-1') {
+	// 		var error = true;
+	// 		$('#email').css("border-color", "#D8000C");
+	// 	} else {
+	// 		$('#email').css("border-color", "#666");
+	// 	}
+	// 	if (subject.length == 0) {
+	// 		var error = true;
+	// 		$('#subject').css("border-color", "#D8000C");
+	// 	} else {
+	// 		$('#subject').css("border-color", "#666");
+	// 	}
+	// 	if (message.length == 0) {
+	// 		var error = true;
+	// 		$('#message').css("border-color", "#D8000C");
+	// 	} else {
+	// 		$('#message').css("border-color", "#666");
+	// 	}
+	// 	if (e_mail.length != 0) {
+	// 		// bot found!
+	// 		var error = true;
+	// 		// but pretend everything went smoothly
+	// 		$('#contact-submit').attr({
+	// 			'disabled': 'false',
+	// 			'value': 'Sending Mail...'
+	// 		});
+	// 		$('#cf-submit').remove();
+	// 		$('#mail-success').fadeIn(500);
+	// 	}
 
-		//now when the validation is done we check if the error variable is false (no errors)
-		if (error == false) {
-			//disable the submit button to avoid spamming
-			//and change the button text to Sending...
-			$('#contact-submit').attr({
-				'disabled': 'false',
-				'value': 'Sending Mail...'
-			});
+	// 	//now when the validation is done we check if the error variable is false (no errors)
+	// 	if (error == false) {
+	// 		//disable the submit button to avoid spamming
+	// 		//and change the button text to Sending...
+	// 		$('#contact-submit').attr({
+	// 			'disabled': 'false',
+	// 			'value': 'Sending Mail...'
+	// 		});
 
 
-			/* using the jquery's post(ajax) function and a lifesaver
-			function serialize() which gets all the data from the form
-			we submit it to send_email.php */
-			var action = $('#contact-form').attr('action');
-			$.post(action, $("#contact-form").serialize(), function () {}, "json").always(function( response ) {
-				//and after the ajax request ends we check the text returned
-				if (response.statusText == 'OK') {
-					//if the mail is sent remove the submit paragraph
-					$('#cf-submit').remove();
-					//and show the mail success div with fadeIn
-					$('#mail-success').fadeIn(500);
-				} else {
-					//show the mail failed div
-					$('#mail-fail').fadeIn(500);
-					//re enable the submit button by removing attribute disabled and change the text back to Send The Message
-					$('#contact-submit').removeAttr('disabled').attr('value', 'Send Mail');
-				}
-			});
+	// 		/* using the jquery's post(ajax) function and a lifesaver
+	// 		function serialize() which gets all the data from the form
+	// 		we submit it to send_email.php */
+	// 		var action = $('#contact-form').attr('action');
+	// 		$.post(action, $("#contact-form").serialize(), function () {}, "json").always(function( response ) {
+	// 			//and after the ajax request ends we check the text returned
+	// 			if (response.statusText == 'OK') {
+	// 				//if the mail is sent remove the submit paragraph
+	// 				$('#cf-submit').remove();
+	// 				//and show the mail success div with fadeIn
+	// 				$('#mail-success').fadeIn(500);
+	// 			} else {
+	// 				//show the mail failed div
+	// 				$('#mail-fail').fadeIn(500);
+	// 				//re enable the submit button by removing attribute disabled and change the text back to Send The Message
+	// 				$('#contact-submit').removeAttr('disabled').attr('value', 'Send Mail');
+	// 			}
+	// 		});
 
-			// var action = $('#contact-form').attr('action');
-			// /* using the jquery's post(ajax) function and a lifesaver
-			// function serialize() which gets all the data from the form
-			// we submit it to send_email.php */
-   //         	$.ajax({
-   //             type: "POST",
-   //             url: action,
-   //             crossDomain: true,
-   //             data: new FormData(this),
-   //             dataType: "json",
-   //             contentType: 'multipart/form-data',
-   //             processData: false,
-   //             contentType: false,
-   //             success: function(response){
-   //               if(response.status == "success"){
-			// 		//if the mail is sent remove the submit paragraph
-			// 		$('#cf-submit').remove();
-			// 		//and show the mail success div with fadeIn
-			// 		$('#mail-success').fadeIn(500);
-			// 	} else {
-			// 		//show the mail failed div
-			// 		$('#mail-fail').fadeIn(500);
-			// 		//re enable the submit button by removing attribute disabled and change the text back to Send The Message
-			// 		$('#contact-submit').removeAttr('disabled').attr('value', 'Send Mail');
-			// 	}
-   //           }
-   //         });
+	// 		// var action = $('#contact-form').attr('action');
+	// 		// /* using the jquery's post(ajax) function and a lifesaver
+	// 		// function serialize() which gets all the data from the form
+	// 		// we submit it to send_email.php */
+ //   //         	$.ajax({
+ //   //             type: "POST",
+ //   //             url: action,
+ //   //             crossDomain: true,
+ //   //             data: new FormData(this),
+ //   //             dataType: "json",
+ //   //             contentType: 'multipart/form-data',
+ //   //             processData: false,
+ //   //             contentType: false,
+ //   //             success: function(response){
+ //   //               if(response.status == "success"){
+	// 		// 		//if the mail is sent remove the submit paragraph
+	// 		// 		$('#cf-submit').remove();
+	// 		// 		//and show the mail success div with fadeIn
+	// 		// 		$('#mail-success').fadeIn(500);
+	// 		// 	} else {
+	// 		// 		//show the mail failed div
+	// 		// 		$('#mail-fail').fadeIn(500);
+	// 		// 		//re enable the submit button by removing attribute disabled and change the text back to Send The Message
+	// 		// 		$('#contact-submit').removeAttr('disabled').attr('value', 'Send Mail');
+	// 		// 	}
+ //   //           }
+ //   //         });
 
-		}
-	});
+	// 	}
+	// });
 
 });
 // End Jquery Function
